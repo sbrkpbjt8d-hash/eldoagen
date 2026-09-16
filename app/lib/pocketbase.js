855706655e4062ef7a54dfd3445164f4d7547d16
MyName <myemail@example.com>
2026-09-16 05:21:20 -0700

    Update project files

--- app/lib/pocketbase.js
+++ app/lib/pocketbase.js
@@ -4,7 +4,7 @@ const getPocketBaseUrl = () => {
   if (typeof window !== 'undefined') {
     // بياخد الـ IP بتاع جهازك تلقائياً سواء فتحت من اللابتوب أو من الموبايل
     const hostname = window.location.hostname;
-    return `http://${hostname}:8091`;
+    return `http://${hostname}:8090`;
   }
   return 'http://127.0.0.1:8090';
 };
